8cbd99feb98d110292708cb509ae7d7f795c1593
zhyingkun <1464959348@qq.com>
2019-10-03 19:12:56 +0800

    Header config.h for tcc

---
+++ libtcc/src/macosx/config.h
@@ -0,0 +1,4 @@
+#ifndef CONFIG_TCCDIR
+#define CONFIG_TCCDIR "/usr/local/zyk/lib/tcc"
+#endif
+#define TCC_VERSION "0.9.27"
---
+++ libtcc/src/win32/config.h
@@ -0,0 +1,4 @@
+#ifndef CONFIG_TCCDIR
+#define CONFIG_TCCDIR "D:/Applications/zyk/lib/tcc"
+#endif
+#define TCC_VERSION "0.9.27"
