9cb9fb6ee3d57abf4d621de41a043c224ef05dcf
zhyingkun <1464959348@qq.com>
2019-10-04 17:25:25 +0800

    Format windows examples source files

--- examples/win32/examples/hello_dll.c
+++ examples/win32/examples/hello_dll.c
@@ -5,16 +5,11 @@
 
 #include <windows.h>
 
-void hello_func (void);
-__declspec(dllimport) extern const char *hello_data;
+void hello_func(void);
+__declspec(dllimport) extern const char* hello_data;
 
-int WINAPI WinMain(
-    HINSTANCE hInstance,
-    HINSTANCE hPrevInstance,
-    LPSTR     lpCmdLine,
-    int       nCmdShow)
-{
-    hello_data = "Hello World!";
-    hello_func();
-    return 0;
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+  hello_data = "Hello World!";
+  hello_func();
+  return 0;
 }
