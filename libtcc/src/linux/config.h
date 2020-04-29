a848d8d1a282e0e773934555bd5d39782eb3bd5d
zhyingkun <1464959348@qq.com>
2020-04-29 22:10:18 +0800

    Update tcc config path

--- libtcc/src/linux/config.h
+++ libtcc/src/linux/config.h
@@ -1,4 +1,4 @@
 #ifndef CONFIG_TCCDIR
-#define CONFIG_TCCDIR "/usr/local/zyk/lib/tcc"
+#define CONFIG_TCCDIR "/usr/local/zyk/tcc/lib/tcc"
 #endif
 #define TCC_VERSION "0.9.27"
--- libtcc/src/win32/config.h
+++ libtcc/src/win32/config.h
@@ -1,4 +1,4 @@
 #ifndef CONFIG_TCCDIR
-#define CONFIG_TCCDIR "D:/Applications/zyk/lib/tcc"
+#define CONFIG_TCCDIR "D:/Applications/zyk/tcc/lib/tcc"
 #endif
 #define TCC_VERSION "0.9.27"
